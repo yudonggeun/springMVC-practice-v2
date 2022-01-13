cf84ce0f83f80d4d84e8c2fdc14e48266c2b094e
유동근 <ydong98@gmailcom>
2022-01-13 10:50:25 +0900

    22-01-12 step 1

---
+++ src/main/java/com/example/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,57 @@
+package com.example.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+@Slf4j
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request,
+                               HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        Integer age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username = {}, age = {}", username, age);
+
+        PrintWriter writer = response.getWriter();
+        writer.write("ok");
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String username,
+                                 @RequestParam("age") int age) {
+        log.info("username = {}, age = {}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+        log.info("username = {}, age = {}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+
+        return "ok";
+    }
+
+}
